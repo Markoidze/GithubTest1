3418e0f3295257aa3f19e93745e2e96eb58f47b1
Markoidze <manucharmarkoidze@gmail.com>
2019-11-17 15:22:12 -0500

    This is my first branch

--- src/Day34/Branch2.java
+++
@@ -1,7 +0,0 @@
-package Day34;
-
-public class Branch2 {
-    public static void main(String[] args) {
-        // Branch 2
-    }
-}
--- src/Day35/Branch3.java
+++
@@ -1,7 +0,0 @@
-package Day35;
-
-public class Branch3 {
-    public static void main(String[] args) {
-        // Branch 3
-    }
-}
