b55e9edb2680c33ae2fa4c93f236b0ebe6a114f7
Markoidze <manucharmarkoidze@gmail.com>
2019-11-17 15:43:16 -0500

    This is my car branch

---
+++ src/CarBranch.java
@@ -0,0 +1,7 @@
+public class CarBranch {
+    public static <Car> void main(String[] args) {
+        // how to use static field, attribute
+
+        System.out.println("This is my car branch");
+    }
+}
