ad67973d9250a785103d4d52099ab5d28da6e01f
Markoidze <manucharmarkoidze@gmail.com>
2019-11-17 22:27:18 -0500

    added some new methods

---
+++ src/pet.java
@@ -0,0 +1,17 @@
+public class pet {
+    public String name;
+    public String breed;
+    public int legs;
+    public int speed;
+    public String gender;
+
+    public void canJump(){
+
+    }
+    public void canSwim(){
+
+    }
+    public void canFly(){
+
+    }
+}
