7dceb4ee377e4f61b50c81d3427e741279ae286f
Markoidze <manucharmarkoidze@gmail.com>
2019-11-17 22:22:48 -0500

    added some methods and .....

---
+++ src/car.java
@@ -0,0 +1,17 @@
+public class car {
+    public String name;
+    private String color;
+    public int engine;
+    private int doors;
+    public int speed;
+
+    public void drive(){
+
+    }
+    public void isElectric(){
+
+    }
+    public void isFest(){
+
+    }
+}
